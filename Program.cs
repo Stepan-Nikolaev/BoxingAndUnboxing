ead3f64cf122b2fc4d6e580f9d658c3abd53bf65
Stepan-Nikolaev <me.ctenah@gmail.com>
2021-07-28 16:41:01 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timeBoxing = new Stopwatch();
+            Stopwatch timeUnboxing = new Stopwatch();
+            int RandomNumber = 10;
+
+            timeBoxing.Start();
+            object box = RandomNumber;
+            timeBoxing.Stop();
+            Console.WriteLine(timeBoxing.ElapsedMilliseconds); // Время выполнения упаковки - 9623 мс
+
+            timeUnboxing.Start();
+            RandomNumber = (int)box;
+            timeUnboxing.Stop();
+            Console.WriteLine(timeUnboxing.ElapsedMilliseconds); // Время выполнения распаковки - 6082 мс
+
+            Console.ReadKey();
+
+        }
+    }
+}
